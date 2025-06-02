0e42d752454f9f72362658829dddc854217e20b3
wionquintela <wionquintela06@gmail.com>
2025-06-02 19:39:45 +0800

    added mission vision

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import MissionVision from "./components/MissionVision";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <MissionVision />
     </div>
   );
 }
---
+++ src/components/MissionVision.js
@@ -0,0 +1,19 @@
+export default function MissionVission() {
+    return (
+        <div className="flex justify-center items-center m-5 sm:flex-row flex-col">
+            <div className=" sm:w-1/3 flex justify-center items-center flex-col text-center pl-4 pr-4 mb-4">
+                <h1 className="text-[25px]">Our Mission</h1>
+                <p className="text-slate-700">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+             <div className=" sm:w-1/3 flex justify-center items-center flex-col text-center pl-4 pr-4 mb-4">
+                <h1 className="text-[25px]">Our Vision</h1>
+                <p className="text-slate-700">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+             <div className=" sm:w-1/3 flex justify-center items-center flex-col text-center pl-4 pr-4 mb-4">
+                <h1 className="text-[25px]">Our Core Values</h1>
+                <p className="text-slate-700">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+             
+        </div>
+    )
+}
