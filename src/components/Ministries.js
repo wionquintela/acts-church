e0aad1e678026bc0d0d07c4e4594fc1b849d0940
wionquintela <wionquintela06@gmail.com>
2025-06-26 18:18:02 +0800

    added ministries

--- src/components/Ministries.js
+++ src/components/Ministries.js
@@ -1,26 +1,40 @@
-import bg from "./assets/Bg.png";
+import img from "./assets/Bg.png";
+import Footer from "./Footer";
 
 export default function Ministries() {
+    const data = [
+        {   
+            title: "Women's Ministry",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+         {   
+            title: "Men's Ministry",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+         {   
+            title: "Youth's Ministry",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+         {   
+            title: "Music Ministry",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+    ]
     return(
-       <div className="relative flex justify-center items-center flex-col  p-10 sm:p-[100px]" 
-               style={{
-                   backgroundImage: `url(${bg})`,
-                    backgroundSize: 'cover',
-                       backgroundAttachment: 'fixed',
-                       backgroundPosition: 'center',
-                       height: '70vh',
-               }}>
-                   {/*Gradient*/}
-                <div className="absolute inset-0 bg-gradient-to-b from-blue-600/60 to-blue-300/40"></div>
-                {/*Content*/}
-                <div className="relative z-10 border">
-                       <h4 className="text-white">Welcome to</h4>
-                       <h1 className="text-[50px] text-white drop-shadow-lg font-semibold"> ACTS Christian Church</h1>
-                       {/* <a className="mt-5 bg-blue-600 hover:bg-blue-700 border-blue-900 border-md text-white pr-5 pl-5 pt-2 pb-2 rounded-lg shadow-lg font-medium" href="/#events">Check Events</a> */}
-                       {/* <button className="text-md border border-2 rounded-sm p-1 border-green-500 text-green-500 hover:bg-green hover:text-white delay-75 mt-4 font-semibold">
-                           More
-                       </button> */}
-               </div>
-               </div>
+        <div>
+            <div className="flex flex-col p-5 pb-[200px]">
+              <div className="p-1 m-2 self-end flex justify-center items-center"><a href="/" className="text-sm underline hover:text-green-800"><i className="fa-solid fa-house fa-xs"></i>Back to Homepage</a></div>
+            <div className="flex justify-center items-center flex-col ">
+                {data.map((data, index) => (
+                     <div className="border shadow flex justify-center items-center w-full flex-col sm:flex-row m-2 rounded-md">
+                    <div className={`${index % 2 == 1 ? "sm:order-2" : ""} w-full sm:w-1/2`}><img src={img} className={`w-full ${index % 2 == 1 ? "sm:rounded-l-lg" : "sm:rounded-r-lg"}`}/></div>
+                    <div className={`p-5 w-full sm:w-1/2 `}><h2 className="text-center text-[35px]">{data.title}</h2>
+                    <p className="text-[13px] text-slate-600">{data.desc}</p></div>
+                </div>
+                ))}
+            </div>
+        </div>
+        <Footer />
+        </div>
     )
 }
